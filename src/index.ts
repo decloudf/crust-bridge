736f8eaa1fdd875e9359feb7451538c7b69cc068
Zikun Fan <zikunvan@gmail.com>
2021-01-19 16:40:05 +0800

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import logger from './log';
 import router from './routes';
 
 const koa = new Koa();
+const cors = require('@koa/cors');
 
 koa.use(router.routes());
+koa.use(cors());
 
 if (require.main === module) {
   logger.info(`🌉  Crust bridge runs on ${process.env.PORT}`);
