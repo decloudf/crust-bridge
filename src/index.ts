b7c732d7130cf7420aa196543b3e894563dd49f1
Zikun Fan <zikunvan@gmail.com>
2021-01-20 16:52:44 +0800

    Add auth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Koa = require('koa');
 import logger from './log';
 import router from './routes';
+import {apiPass} from './env';
 
 const app = new Koa();
 const cors = require('@koa/cors');
+const auth = require('koa-basic-auth');
 
 app.use(cors());
 
@@ -12,12 +14,20 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (e) {
-    logger.error(`💥 Unknown global error catched: ${JSON.stringify(e)}`);
-    ctx.status = e.statusCode || e.status || 500;
-    ctx.body = {msg: 'Unknown error'};
+    if (401 === e.status) {
+      ctx.status = 401;
+      ctx.body = {msg: 'You have no access.'};
+    } else {
+      logger.error(`💥 Unknown global error catched: ${JSON.stringify(e)}`);
+      ctx.status = e.statusCode || e.status || 500;
+      ctx.body = {msg: 'Unknown error'};
+    }
     ctx.app.emit('error', e, ctx);
   }
 });
+
+// Authentication
+app.use(auth(apiPass));
 
 app.use(router.routes());
 
