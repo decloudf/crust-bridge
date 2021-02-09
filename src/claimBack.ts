d511cb70288daf7aa05752934cc80fe83dc619b6
Zikun Fan <zikunvan@gmail.com>
2021-02-09 15:52:17 +0800

    Add get eth bonding and sum up with amount

--- src/claimBack.ts
+++ src/claimBack.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-exit */
 import {
   claimBackAddress,
   claimBackEndBN,
@@ -8,12 +9,26 @@ import got from 'got';
 import logger from './log';
 import {parseObj} from './util';
 import * as _ from 'lodash';
+import {getApi} from './services/crustApi';
 const sleep = require('util').promisify(setTimeout);
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const subscanEndpoint = 'https://crust.subscan.io/api/scan/transfers';
 const row = 20;
+const api = getApi();
 
-async function scanClaimBack(): Promise<any[]> {
+interface ClaimBackTransfer {
+  account: string;
+  amount: number;
+}
+
+interface ClaimBackRecord {
+  crustAccount: string;
+  ethAccount: string;
+  amount: number;
+}
+
+async function getClaimBackTransfers(): Promise<ClaimBackTransfer[]> {
   const res: any = await got.post(subscanEndpoint, {
     json: {
       address: claimBackAddress,
@@ -49,23 +64,87 @@ async function scanClaimBack(): Promise<any[]> {
   const toTargetTransfers = totalTransfers.filter(
     t =>
       t.to === claimBackAddress &&
-      t.amount > claimBackFee &&
-      t.block_num >= claimBackStartBN &&
-      t.block_num < claimBackEndBN
+      t.block_num > claimBackStartBN &&
+      t.block_num <= claimBackEndBN
   );
 
-  toTargetTransfers.forEach(tt => {
-    logger.info(`${tt.from} -> ${tt.to}: ${tt.amount} CRUs`);
+  toTargetTransfers.forEach((tt, idx) => {
+    logger.info(
+      `${idx}: ${tt.from} -> ${tt.to}(BN: ${tt.block_num}): ${tt.amount} CRUs`
+    );
   });
 
-  return toTargetTransfers;
+  return toTargetTransfers.map(tt => {
+    const cbt: ClaimBackTransfer = {
+      account: tt.from,
+      amount: tt.amount,
+    };
+    return cbt;
+  });
 }
 
-scanClaimBack()
+async function getBondedEth(account: string): Promise<string | null> {
+  const maybeEthAddr = parseObj(await api.query.claims.bondedEth(account));
+
+  return maybeEthAddr ? maybeEthAddr : null;
+}
+
+async function main(): Promise<boolean> {
+  // 0. Get all legal claim back transfers
+  const claimBackTransfers: ClaimBackTransfer[] = await getClaimBackTransfers();
+  const claimBackMap: Map<string, [string | null, number]> = new Map();
+
+  // 1. Get bonded ethereum address and sum up by same `crustAccount`
+  for (const cbt of claimBackTransfers) {
+    const crustAccount = cbt.account;
+    if (claimBackMap.has(crustAccount)) {
+      // a. Sum up with existed account
+      const value = claimBackMap.get(crustAccount);
+      if (value) {
+        const amount = Number(value[1]) + Number(cbt.amount);
+        claimBackMap.set(crustAccount, [value[0], amount]);
+      }
+    } else {
+      // b. Insert with new account
+      const maybeEthAddr = await getBondedEth(cbt.account);
+      claimBackMap.set(crustAccount, [maybeEthAddr, cbt.amount]);
+    }
+  }
+
+  // 2. Construct records
+  const claimBackRecords: ClaimBackRecord[] = [];
+  for (const crustAccount of claimBackMap.keys()) {
+    const value = claimBackMap.get(crustAccount);
+    if (value) {
+      claimBackRecords.push({
+        crustAccount,
+        ethAccount: value[0] !== null ? value[0] : '',
+        amount: value[1] - claimBackFee,
+      });
+    }
+  }
+
+  // 3. Write to csv file
+  const csvWriter = createCsvWriter({
+    path: `./crust-bridge-claim-back-${new Date().toISOString()}.csv`,
+    header: [
+      {id: 'crustAccount', title: 'Crust Address'},
+      {id: 'ethAccount', title: 'Ethereum Address'},
+      {id: 'amount', title: 'Amount'},
+    ],
+  });
+
+  await csvWriter.writeRecords(claimBackRecords);
+
+  return true;
+}
+
+main()
   .then(r => {
-    //logger.info(JSON.stringify(r));
+    logger.info('Parse claim back txs success');
+    process.exit(0);
   })
   .catch(e => {
-    logger.error(e.toString());
-    return -1;
+    logger.error(`Parse finished with error: ${e}`);
+    process.exit(1);
   });
