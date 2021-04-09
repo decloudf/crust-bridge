ca55315e7d011842c47aab91979fa1100ff3e15e
Kun <zikunvan@gmail.com>
2021-04-09 18:44:02 +0800

    Reorg project layout (#13)
    * Reorg project layout
    
    * Fix CI
    
    * Add name to CI steps
    
    * Add prettierrc.js
    
    * Add mainnet bridge base structure
    
    * Add build command to mainnet bridge

--- maxwell-bridge/dockerize.sh
+++ maxwell-bridge/dockerize.sh
@@ -6,6 +6,6 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-IMAGEID="crustio/crust-bridge:$PACKAGE_VERSION"
-echo "Building crustio/crust-bridge:$PACKAGE_VERSION ..."
+IMAGEID="crustio/crust-maxwell-bridge:$PACKAGE_VERSION"
+echo "Building crustio/crust-maxwell-bridge:$PACKAGE_VERSION ..."
 docker build -t $IMAGEID .
