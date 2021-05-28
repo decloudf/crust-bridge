0fbd2b3abe61cb177294e0d5f2f14e76c4df47b8
Yashiro <51014425+yuhui1208@users.noreply.github.com>
2021-05-28 11:52:03 +0800

    Add maxwell csm claim (#17)
    * Add maxwell csm claim
    
    * Fix function name
    
    * Fix type version
    
    * Fix mint func name
    
    * Fix start script
    
    * Fix
    
    * Add port to env example

---
+++ maxwell-csm-claim/src/env.ts
@@ -0,0 +1,19 @@
+// Load env
+
+// eslint-disable-next-line node/no-extraneous-require
+require('dotenv').config();
+
+export const csmABI = require('./abi');
+export const ethEndpoint = process.env.ETH_ENDPOINT as string;
+export const csmContractAddr = process.env.CSM_CONTRACT_ADDRESS as string;
+export const minerSeeds = process.env.MINER_SEEDS as string;
+export const cruEndpoint = process.env.CRU_ENDPOINT as string;
+export const csmClaimAddr = process.env.CSM_CLAIM_ADDRESS as string;
+export const minEthConfirmation = Number(
+  process.env.MIN_ETH_CONFIRMATION as string
+);
+// export const csmClaimContract = process.env.CSM_CLAIM_CONTRACT as string;
+export const apiPass = {
+  name: process.env.API_AUTH_NAME as string,
+  pass: process.env.API_AUTH_PWD as string,
+};
---
+++ maxwell-csm-claim/src/router.ts
@@ -0,0 +1,85 @@
+import {Context, Next} from 'koa';
+import BN = require('bn.js');
+import Router = require('koa-router');
+import {mintCsmClaim, ethTxParser} from './crustApi';
+import logger from './log';
+import {timeout} from 'promise-timeout';
+
+const router = new Router();
+const txLocker = {};
+
+router.post('/csmClaim/:hash', async (ctx: Context, next: Next) => {
+  const ethTxHash = ctx.params.hash;
+  const parseTx = async () => {
+    logger.info(`Received eth tx hash: ${ethTxHash}`);
+    // 1. Parse eth tx
+    const parseRes: [string, BN] | null = await ethTxParser(ethTxHash);
+
+    // Illegal crust claim transaction
+    if (!parseRes) {
+      return false;
+    } else {
+      // 2. Mint into crust maxwell
+      const claimer = parseRes[0];
+      const amount = parseRes[1];
+
+      return await handleWithLock(
+        ctx,
+        txLocker,
+        'sendMintClaim',
+        async () => {
+          return await mintCsmClaim(ethTxHash, claimer, amount);
+        },
+        {
+          code: 409,
+          msg: 'MintClaimIsOccupied',
+        }
+      );
+    }
+  };
+
+  const result = await handleWithLock(ctx, txLocker, ethTxHash, parseTx, {
+    code: 423,
+    msg: 'SameEthTxIsHandling',
+  });
+
+  if (result) {
+    ctx.response.status = 200;
+    ctx.response.body = {
+      msg: 'MintClaimSuccess',
+    };
+  } else {
+    ctx.response.status = 400;
+    ctx.response.body = {
+      msg: 'MintClaimFailed',
+    };
+  }
+  await next();
+});
+
+async function handleWithLock(
+  ctx: Context,
+  lockCtx: any,
+  key: string,
+  handler: Function,
+  error: any
+) {
+  logger.debug(`Handle with locking key: ${key}`);
+  if (lockCtx[key]) {
+    return ctx.throw(error.code || 409, error);
+  }
+
+  try {
+    lockCtx[key] = true;
+    return await timeout(
+      new Promise((resolve, reject) => {
+        handler().then(resolve).catch(reject);
+      }),
+      2 * 60 * 1000 // 2 min will timeout
+    );
+  } finally {
+    delete lockCtx[key];
+  }
+}
+
+export default router;
