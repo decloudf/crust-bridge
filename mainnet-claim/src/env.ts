47b807faa0497afaa05b37e9d35427d6916bd2ea
Kun <zikunvan@gmail.com>
2021-07-28 19:28:22 +0800

    Add mainnet claim (#18)
    * Init mainnet claim
    
    * Fix mintClaim return value
    
    * Optimize log
    
    * Delete irrelavant lig
    
    * Upgrade some library and trim the input tx hash
    
    * Add CSM claim back in Maxwell

--- mainnet-claim/src/env.ts
+++ mainnet-claim/src/env.ts
@@ -6,10 +6,8 @@ require('dotenv').config();
 export const crustABI = require('./abi');
 export const ethEndpoint = process.env.ETH_ENDPOINT as string;
 export const cruContractAddr = process.env.CRU_CONTRACT_ADDRESS as string;
-export const cruClaimAddr = process.env.CRU_CLAIM_ADDRESS as string;
-export const cruClaimContract = process.env.CRU_CLAIM_CONTRACT as string;
+export const cruBurnAddr = process.env.CRU_BURN_ADDRESS as string;
 export const minerSeeds = process.env.MINER_SEEDS as string;
-export const minerPwd = process.env.MINER_PWD as string;
 export const cruEndpoint = process.env.CRU_ENDPOINT as string;
 export const minEthConfirmation = Number(
   process.env.MIN_ETH_CONFIRMATION as string
@@ -18,10 +16,3 @@ export const apiPass = {
   name: process.env.API_AUTH_NAME as string,
   pass: process.env.API_AUTH_PWD as string,
 };
-export const claimBackStartBN = Number(
-  process.env.CLAIM_BACK_START_BN as string
-);
-export const claimBackEndBN = Number(process.env.CLAIM_BACK_END_BN as string);
-export const cruClaimBackFee = Number(process.env.CRU_CLAIM_BACK_FEE as string);
-export const csmClaimBackFee = Number(process.env.CSM_CLAIM_BACK_FEE as string);
-export const claimBackAddress = process.env.CLAIM_BACK_ADDRESS as string;
---
+++ mainnet-claim/src/index.ts
@@ -0,0 +1,37 @@
+import Koa = require('koa');
+import logger from './log';
+import router from './routes';
+import {apiPass} from './env';
+
+const app = new Koa();
+const cors = require('@koa/cors');
+const auth = require('koa-basic-auth');
+
+app.use(cors());
+
+// Global error handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (e) {
+    if (401 === e.status) {
+      ctx.status = 401;
+      ctx.body = {msg: 'You have no access.'};
+    } else {
+      logger.error(`💥 Unknown global error catched: ${JSON.stringify(e)}`);
+      ctx.status = e.statusCode || e.status || 500;
+      ctx.body = {msg: 'Unknown error'};
+    }
+    ctx.app.emit('error', e, ctx);
+  }
+});
+
+// Authentication
+app.use(auth(apiPass));
+
+app.use(router.routes());
+
+if (require.main === module) {
+  logger.info(`🌉  Crust Maxwell Bridge runs on ${process.env.PORT}`);
+  app.listen(process.env.PORT); // default ports
+}
