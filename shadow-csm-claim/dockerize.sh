02f0c5e18f29e057f4e9dfc14f9c718ad75f383f
yuhui <1161569428@qq.com>
2022-04-13 13:17:56 +0800

    Fix dockerize

--- shadow-csm-claim/dockerize.sh
+++ shadow-csm-claim/dockerize.sh
@@ -6,6 +6,6 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-IMAGEID="crustio/crust-mainnet-claim:$PACKAGE_VERSION"
-echo "Building crustio/crust-mainnet-claim:$PACKAGE_VERSION ..."
+IMAGEID="crustio/crust-shadow-claim:$PACKAGE_VERSION"
+echo "Building crustio/crust-shadow-claim:$PACKAGE_VERSION ..."
 docker build -t $IMAGEID .
